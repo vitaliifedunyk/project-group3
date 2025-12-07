ec2af2913d4074b1337e125f9590186bc10b5557
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2025-12-07 22:12:47 +0100

    upd js file

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ const menu = document.querySelector('[data-menu]');
 const menuOpenBtn = document.querySelector('[data-menu-open]');
 const menuCloseBtn = document.querySelector('[data-menu-close]');
 
-if (menuOpenBtn) {
+if (menuOpenBtn && menu) {
   menuOpenBtn.addEventListener('click', () => {
     menu.classList.add('is-open');
     document.body.style.overflow = 'hidden';
   });
 }
 
-if (menuCloseBtn) {
+if (menuCloseBtn && menu) {
   menuCloseBtn.addEventListener('click', () => {
     menu.classList.remove('is-open');
     document.body.style.overflow = '';
